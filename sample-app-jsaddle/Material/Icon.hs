1891706462efaf870aaa057934462c7a289d8fe1
Martin Troiber <martin.troiber@gmail.com>
2020-07-06 03:06:46 +0200

    add icons

---
+++ sample-app-jsaddle/Material/Icon.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Material.Icon(icon) where
+
+import Miso.Html
+import Miso.String
+
+
+{-| Icon view function
+-}
+icon :: [Attribute msg] -> String -> View msg
+icon additionalAttributes iconName =
+    i_ (class_ "material-icons":additionalAttributes) [ text (toMisoString iconName) ]
