f698e3d7c68fae81149af6118031109d00a40e7c
Martin Troiber <martin.troiber@gmail.com>
2020-07-06 17:14:17 +0200

    update Miso version

--- sample-app-jsaddle/default.nix
+++ sample-app-jsaddle/default.nix
@@ -1,6 +1,6 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/ea25964565074e73d4052b56b60b6e101fa08bc5.tar.gz";
-  sha256 = "1yb9yvc0ln4yn1jk2k5kwwa1s32310abawz40yd8cqqkm1z7w6wg";
+  url = "https://github.com/dmjio/miso/archive/1.7.1.tar.gz";
+  sha256 = "0q44lxzz8pp89ccaiw3iwczha8x2rxjwmgzkxj8cxm97ymsm0diy";
 }) {});
 {
   dev = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. { miso = miso-jsaddle; };
