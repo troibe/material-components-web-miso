d80e53705425dbddcf6e173f0f0d69bb7ea36102
Martin Troiber <martin.troiber@gmail.com>
2020-12-13 22:32:54 +0100

    Add Input ChipSet implementation

---
+++ sample-app-jsaddle/Material/Chip/Input/Internal.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Material.Chip.Input.Internal  (Chip(..), Config(..), Icon(..)) where
+
+
+import qualified Data.Void as Void
+import qualified Miso
+
+
+data Config msg
+    = Config
+        { leadingIcon :: Maybe Icon
+        , trailingIcon :: Maybe Icon
+        , additionalAttributes :: [Miso.Attribute msg]
+        , onClick :: Maybe msg
+        , onDelete :: Maybe msg
+        }
+
+
+data Chip msg
+    = Chip (Config msg) String
+
+
+data Icon
+    = Icon
+        { node :: [Miso.Attribute Void.Void] -> [Miso.View Void.Void] -> Miso.View Void.Void
+        , attributes :: [Miso.Attribute Void.Void]
+        , nodes :: [Miso.View Void.Void]
+        }
+    | SvgIcon
+        { node :: [Miso.Attribute Void.Void] -> [Miso.View Void.Void] -> Miso.View Void.Void
+        , attributes :: [Miso.Attribute Void.Void]
+        , nodes :: [Miso.View Void.Void]
+        }
