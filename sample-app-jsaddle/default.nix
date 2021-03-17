3088ec528505a9a59854a2fb3f019d7629463a5d
developandplay <34752929+developandplay@users.noreply.github.com>
2021-03-17 13:24:10 +0100

    Upgrade Miso version to allow props with zero value

--- sample-app-jsaddle/default.nix
+++ sample-app-jsaddle/default.nix
@@ -1,6 +1,6 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/1.7.1.tar.gz";
-  sha256 = "0q44lxzz8pp89ccaiw3iwczha8x2rxjwmgzkxj8cxm97ymsm0diy";
+  url = "https://github.com/dmjio/miso/archive/bd8c216922f8a01d068a20e5f335761d6635807d.tar.gz";
+  sha256 = "1g19jz32nah064b4948mlinv7jkj5840j3q4khx43i3iznf0rkwa";
 }) {});
 {
   dev = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. { miso = miso-jsaddle; };
